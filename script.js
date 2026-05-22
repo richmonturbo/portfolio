475f8a80ba11ffa24100d0206ba4be0b9ac4e4ef
richmonturbo <monjarazrich@gmail.com>
2026-05-22 12:23:35 -0700

    Update script.js

--- script.js
+++ script.js
@@ -81,3 +81,20 @@ function changeImage(arrow, direction) {
   card.dataset.index = index;
   img.src = images[index];
 }
+
+const backToTop = document.getElementById("backToTop");
+
+window.onscroll = function () {
+  if (document.documentElement.scrollTop > 200) {
+    backToTop.style.display = "block";
+  } else {
+    backToTop.style.display = "none";
+  }
+};
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+}
